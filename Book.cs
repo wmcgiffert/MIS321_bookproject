a03a257655ebb1c33d7827553a8606581d5c0f86
wmcgiffert <wmcgiffert@crimson.ua.edu>
2020-08-23 19:30:47 -0500

    Initial Commit

---
+++ Book.cs
@@ -0,0 +1,15 @@
+ namespace bookproject
+{
+    public class Book
+    {
+        //Gettters & Setters
+        public string Title         
+        { get; set; }
+       public string Author
+        { get; set; }
+
+        //Constructors
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bookproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book myBook = new Book(){Title = "Mistborn 1", Author = "Brandon Sanderson"};
+
+            Console.WriteLine(myBook.Title);
+            Console.WriteLine(myBook.Author);
+
+
+
+        }
+    }
+}
