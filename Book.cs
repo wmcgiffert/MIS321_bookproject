00e4964d365b52e46830bed41baab6ca7bec8ccf
wmcgiffert <wmcgiffert@crimson.ua.edu>
2020-08-23 20:29:06 -0500

    v1.1

--- Book.cs
+++ Book.cs
@@ -5,7 +5,7 @@
         //Gettters & Setters
         public string Title         
         { get; set; }
-       public string Author
+        public string Author
         { get; set; }
 
         //Constructors
