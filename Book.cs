eb38a002514a42ad713cd9060303038fa21bcd1a
wmcgiffert <wmcgiffert@crimson.ua.edu>
2020-08-26 22:19:36 -0500

    refresher vid 6 done

--- Book.cs
+++ Book.cs
@@ -3,13 +3,11 @@
     public class Book
     {
         //Gettters & Setters
-        public string Title         
-        { get; set; }
-        public string Author
-        { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNums { get; set; }
 
         //Constructors
-
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,10 +7,21 @@ namespace bookproject
         static void Main(string[] args)
         {
 
-            Book myBook = new Book(){Title = "Mistborn 1", Author = "Brandon Sanderson"};
+            
+            Book[] fantasyBooks = new Book[50];
+            fantasyBooks[0] = new Book(){Title = "The Final Empire", Author = "Brandon Sanderson", PageNums = 500};
+            fantasyBooks[1] = new Book(){Title = "Well of Ascension", Author = "Brandon Sanderson", PageNums = 500};
+            fantasyBooks[2] = new Book(){Title = "The Hero of Ages", Author = "Brandon Sanderson", PageNums = 500};
 
-            Console.WriteLine(myBook.Title);
-            Console.WriteLine(myBook.Author);
+            for(int i = 0; i < 3; i++ )
+            {
+                Console.WriteLine($"{fantasyBooks[i].Title} has {fantasyBooks[i].PageNums} pages");    
+            }
+
+
+            
+            
+
 
 
 
